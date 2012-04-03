21bed1a4ebc9b9d8b20e9efa6bde2a941b8a8764
Paul Uithol <paul.uithol@progressivecompany.nl>
2012-04-03 15:43:39 +0200

    Make routing managed by the API

--- tastymongo/api.py
+++ tastymongo/api.py
@@ -4,8 +4,9 @@ from __future__ import unicode_literals
 import warnings
 
 from pyramid.response import Response
+from pyramid.config import Configurator
 
-from .exceptions import NotRegistered, BadRequest
+from .exceptions import NotRegistered, BadRequest, ConfigurationError
 from .serializers import Serializer
 from .utils import *
 
@@ -22,22 +23,33 @@ class Api(object):
     this is done with version numbers (i.e. ``v1``, ``v2``, etc.) but can
     be named any string.
     """
-    def __init__(self, api_name="v1"):
+    def __init__(self, config, api_name='api', api_version='v1'):
         self.api_name = api_name
+        self.api_version = api_version
         self._registry = {}
+        self.config = config
 
-    def register( self, resource ):
+        self.route = '/{}/{}'.format(self.api_name, self.api_version)
+
+        self.config.add_route(self.route, self.route + '/')
+        self.config.add_view(self.top_level, route_name=self.route)
+
+    @staticmethod
+    def resolve_uri( uri='/' ):
+        pass
+
+    @staticmethod
+    def build_uri( request, route_name, *elements ):
+        return request.route_url( route_name, *elements )
+
+    def register(self, resource):
         """
         Registers an instance of a ``Resource`` subclass with the API.
-
-        Optionally accept a ``canonical`` argument, which indicates that the
-        resource being registered is the canonical variant. Defaults to
-        ``True``.
         """
         resource_name = getattr(resource._meta, 'resource_name', None)
 
         if resource_name is None:
-            raise ImproperlyConfigured("Resource %r must define a 'resource_name'." % resource)
+            raise ConfigurationError("Resource %r must define a 'resource_name'." % resource)
 
         self._registry[resource_name] = resource
 
@@ -45,44 +57,39 @@ class Api(object):
         resource._meta.api_name = self.api_name
         resource.__class__.Meta.api_name = self.api_name
 
+        # add 'list' action
+        list_name = '{}/{}/list'.format(self.route, resource_name)
+        self.config.add_route(list_name, '{}/{}/'.format(self.route, resource_name))
+        self.config.add_view(resource.dispatch_list, route_name=list_name)
+
+        # add 'schema' action
+        schema_name = '{}/{}/schema'.format(self.route, resource_name)
+        self.config.add_route(schema_name, '{}/{}/schema'.format(self.route, resource_name))
+        self.config.add_view(resource.get_schema, route_name=schema_name)
+
+        # add 'get_multiple' action
+        multiple_name = '{}/{}/multiple'.format(self.route, resource_name)
+        self.config.add_route(multiple_name, '{}/{}/set/{{ids}}/'.format(self.route, resource_name))
+        self.config.add_view(resource.get_multiple, route_name=multiple_name)
+
+        # add 'detail' action
+        detail_name = '{}/{}/detail'.format(self.route, resource_name)
+        self.config.add_route(detail_name, '{}/{}/{{id}}/'.format(self.route, resource_name))
+        self.config.add_view(resource.dispatch_detail, route_name=detail_name)
+
     def unregister(self, resource_name):
         """
         If present, unregisters a resource from the API.
         """
         if resource_name in self._registry:
             del(self._registry[resource_name])
-
-        raise NotRegistered("No resource was registered as canonical for '%s'." % resource_name)
+        else:
+            raise NotRegistered("No resource was registered for '%s'." % resource_name)
 
     def wrap_view(self, view):
         def wrapper(request, *args, **kwargs):
             return getattr(self, view)(request, *args, **kwargs)
         return wrapper
-
-    def override_urls(self):
-        """
-        A hook for adding your own URLs or overriding the default URLs.
-        """
-        return []
-
-    @property
-    def urls(self):
-        """
-        Provides URLconf details for the ``Api`` and all registered
-        ``Resources`` beneath it.
-        """
-        pattern_list = [
-            url(r"^(?P<api_name>%s)%s$" % (self.api_name, trailing_slash()), self.wrap_view('top_level'), name="api_%s_top_level" % self.api_name),
-        ]
-
-        for name in sorted(self._registry.keys()):
-            self._registry[name].api_name = self.api_name
-            pattern_list.append((r"^(?P<api_name>%s)/" % self.api_name, include(self._registry[name].urls)))
-
-        urlpatterns = self.override_urls() + patterns('',
-            *pattern_list
-        )
-        return urlpatterns
 
     def top_level(self, request, api_name=None):
         """
@@ -97,11 +104,11 @@ class Api(object):
 
         for name in sorted(self._registry.keys()):
             available_resources[name] = {
-                'list_endpoint': self._build_reverse_url("api_dispatch_list", kwargs={
+                'list_endpoint': self.build_uri(request, "api_dispatch_list", {
                     'api_name': api_name,
                     'resource_name': name,
                 }),
-                'schema': self._build_reverse_url("api_get_schema", kwargs={
+                'schema': self.build_uri(request, "api_get_schema", {
                     'api_name': api_name,
                     'resource_name': name,
                 }),
@@ -112,11 +119,3 @@ class Api(object):
 
         serialized = serializer.serialize(available_resources, desired_format, options)
         return Response( body=serialized, content_type=build_content_type(desired_format) )
-
-    def _build_reverse_url(self, name, args=None, kwargs=None):
-        """
-        A convenience hook for overriding how URLs are built.
-
-        See ``NamespacedApi._build_reverse_url`` for an example.
-        """
-        return reverse(name, args=args, kwargs=kwargs)
