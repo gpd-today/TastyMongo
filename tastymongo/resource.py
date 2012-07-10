2774ebb43ce8d7669035c58a61a629be613a01bb
Marcel van den Elst <marcel@marutec.nl>
2012-07-10 15:57:49 +0200

    REVERTME: this commit must be reverted when RelationalMixin updates its _memo_related_docs on `getattr`
    Implements fetching current related documents and memoizing them before
    setting new documents in the API layer. Quick workaround since the API
    shouldn't know or care about this.

--- tastymongo/resource.py
+++ tastymongo/resource.py
@@ -430,6 +430,12 @@ class Resource( object ):
                         bundle.errors[ field_name ] = related_errors
 
                     if not fld.readonly:
+                        # FIXME: This should move to RelationalMixin:
+                        # Retrieve the current relations and memoize them.
+                        to_memo = getattr(bundle.obj, fld.attribute)
+                        for doc in to_memo:
+                            if doc not in bundle.obj._memo_related_docs:
+                                bundle.obj._memo_related_docs.add(doc)
                         setattr( bundle.obj, fld.attribute, [b.obj for b in data] )
 
                 else:
@@ -438,6 +444,11 @@ class Resource( object ):
                         bundle.errors[ field_name ] = data.errors 
 
                     if not fld.readonly:
+                        # FIXME: This should move to RelationalMixin:
+                        # Retrieve the current relations and memoize them.
+                        doc = getattr(bundle.obj, fld.attribute)
+                        if doc and doc not in bundle.obj._memo_related_docs:
+                            bundle.obj._memo_related_docs.add(doc)
                         if data is not None:
                             setattr( bundle.obj, fld.attribute, data.obj )
                         else:
