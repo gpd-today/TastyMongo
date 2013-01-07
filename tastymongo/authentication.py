65fc747a364db49439341ceb003c0d551d4b0c3f
Paul Uithol <paul.uithol@progressivecompany.nl>
2013-01-07 14:11:47 +0100

    Add an Authentication method that always allows requests

--- tastymongo/authentication.py
+++ tastymongo/authentication.py
@@ -1,13 +1,13 @@
 from __future__ import print_function
 from __future__ import unicode_literals
 
-class Authentication(object):
+class Authentication( object ):
     """
     A simple base class to establish the protocol for auth.
 
     By default, this indicates the user is always authenticated.
     """
-    def is_authenticated(self, request, **kwargs):
+    def is_authenticated( self, request, **kwargs ):
         """
         Identifies if the user is authenticated to continue or not.
 
@@ -16,12 +16,15 @@ class Authentication(object):
         """
         return not not request.user
 
-    def get_identifier(self, request):
+    def get_identifier( self, request ):
         """
         Provides a unique string identifier for the requestor.
 
         This implementation returns a combination of IP address and hostname.
         """
-        return "%s_%s" % (request.remote_addr, request.host)
+        return "%s_%s" % ( request.remote_addr, request.host )
 
 
+class NoAutentication( Authentication ):
+    def is_authenticated( self, request, **kwargs ):
+        return True
