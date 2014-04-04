f496df064376d7137cd38ed859b8b5740aafb4c9
Paul Uithol <paul.uithol@progressivecompany.nl>
2014-04-04 13:04:49 +0200

    Remove some query operators we don't implement from the API (like Geo, match, mod)

--- tastymongo/constants.py
+++ tastymongo/constants.py
@@ -9,15 +9,11 @@ ALL_WITH_RELATIONS = 2
 
 # FIXME: Query terms should be exposed by MongoEngine like in Django.
 # Valid query terms.
-QUERY_OPERATORS = ['ne', 'gt', 'gte', 'lt', 'lte', 'in', 'nin', 'mod',
-             'all', 'size', 'exists', 'not']
-QUERY_GEO_OPERATORS = ['within_distance', 'within_spherical_distance', 'within_box', 'within_polygon', 'near', 'near_sphere']
-QUERY_MATCH_OPERATORS = ['contains', 'icontains', 'startswith',
-                   'istartswith', 'endswith', 'iendswith',
-                   'exact', 'iexact']
-QUERY_CUSTOM_OPERATORS = ['match']
+QUERY_OPERATORS = ['ne', 'gt', 'gte', 'lt', 'lte', 'in', 'nin', 'all', 'size', 'exists']
+# QUERY_GEO_OPERATORS = ['within_distance', 'within_spherical_distance', 'within_box', 'within_polygon', 'near', 'near_sphere']
+QUERY_MATCH_OPERATORS = ['contains', 'icontains', 'startswith', 'istartswith', 'endswith', 'iendswith', 'exact', 'iexact']
 
 # Use a Dict for O(1) lookups and because filters are also specified dictwise
-QUERY_TERMS = dict([(x, None) for x in (QUERY_OPERATORS + QUERY_GEO_OPERATORS + QUERY_MATCH_OPERATORS + QUERY_CUSTOM_OPERATORS)])
+QUERY_TERMS = dict( [(x, None) for x in ( QUERY_OPERATORS + QUERY_MATCH_OPERATORS ) ] )
 
 LOOKUP_SEP = '__'
